bde68cb134eeba920ecb48d80cd2e167925cb306
Luan <luan.soya@gmail.com>
2026-04-22 11:40:29 -0300

    arrumando o  electron

--- blocos/titlebar.tsx
+++ blocos/titlebar.tsx
@@ -1,12 +1,29 @@
-"use client"; // Só este arquivo é Client Component
+"use client";
+
+import { useEffect, useState } from "react";
 
 export function Titlebar() {
+    const [isElectron, setIsElectron] = useState(false);
+
+    useEffect(() => {
+        if (document.documentElement.classList.contains("is-electron")) {
+            setIsElectron(true);
+        }
+    }, []);
+
+    if (!isElectron) return null;
+
     return (
-        <div className="titlebar">
-            <span className="text-xs opacity-70">ZentraX</span>
+        <div className="flex justify-between px-5 py-2 bg-purple-700">
+            <span className="text-base text-blue-100 font-bold">ZentraX</span>
             <div className="window-controls">
-                <button onClick={() => window.electron?.minimize()}>-</button>
-                <button onClick={() => window.electron?.close()}>×</button>
+                <button className="text-xl h-7 w-7 bg-white flex justify-center items-center rounded-3xl cursor-pointer text-purple-700" onClick={() => window.electron?.minimize()}>-</button>
+
+                <button
+                    className="text-sm font-bold h-7 w-7 bg-white flex justify-center items-center rounded-3xl cursor-pointer text-purple-700"
+                    onClick={() => window.electron?.maximize()}>▢</button>
+
+                <button className="text-xl h-7 w-7 bg-white flex justify-center items-center rounded-3xl cursor-pointer text-purple-700" onClick={() => window.electron?.close()}>×</button>
             </div>
         </div>
     );
